5008497622ef02bafa497b1f346352abe090dee1
DuarteEscariz <duarteesca@gmail.com>
2022-04-21 19:57:08 +0200

    Ej 1

---
+++ Ej1/Ej1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ej1
+{
+    class Astro
+    {
+        private String nombre;
+        public String Nombre
+        {
+            set
+            {
+                nombre = value.ToUpper().Trim();
+            }
+            get
+            {
+
+            }
+        }
+        public String get_Nombre(String c)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
