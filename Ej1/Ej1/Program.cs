ceaaf07673609d2663dabf5fde6b51fe7ef1468e
DuarteEscariz <duarteesca@gmail.com>
2022-04-25 19:10:39 +0200

    Ej1 B

--- Ej1/Ej1/Program.cs
+++ Ej1/Ej1/Program.cs
@@ -2,6 +2,10 @@
 
 namespace Ej1
 {
+    public class RadioNegativoException : Exception
+    {
+
+    }
     class Astro
     {
         private String nombre;
@@ -13,13 +17,76 @@ namespace Ej1
             }
             get
             {
-
+                return nombre;
             }
         }
-        public String get_Nombre(String c)
+        public String get_Nombre(Char c)
         {
-
+            String final="";
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                if (i == 0)
+                {
+                    final= final + nombre[i];
+                }
+                else
+                {
+                    final = final + c + nombre[i];
+                }
+            }
+            return final;
         }
+        private int radio;
+        public int Radio
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    radio = value;
+                } 
+                else throw new RadioNegativoException();
+            }
+            get
+            {
+                return radio;
+            }
+        }
+        public override string ToString()
+        {
+            String salida = nombre + " Radio: " + radio.ToString("F2");
+            return salida;
+        }
+        //public override bool Equals(object obj){} TODO
+        public override 
+    }
+    class Planeta : Astro
+    {
+        private bool gaseoso;
+        public bool Gaseoso
+        {
+            get
+            {
+                return gaseoso;
+            }
+            set
+            {
+                gaseoso = value;
+            }
+        }
+        public Astro[] Satelites;
+        public Planeta(String nombre, int radio, bool gaseoso)
+        {
+            this.Nombre = nombre;
+            this.Radio = radio;
+            this.gaseoso = gaseoso;
+            this.Satelites = new Astro[0] ;
+        }
+        public Planeta()
+        {
+            new Planeta("", 0, false);
+        }
+       
     }
     class Program
     {
