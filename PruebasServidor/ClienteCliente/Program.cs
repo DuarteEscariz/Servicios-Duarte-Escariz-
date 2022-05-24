75b45b7f52d03039e026ec1074aa96ffb34e6248
DuarteEscariz <duarteesca@gmail.com>
2022-05-24 19:49:56 +0200

    Servidor

---
+++ PruebasServidor/ClienteCliente/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClienteCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string IP_Server = "127.0.0.1";
+            string msg;
+            string userMsg;
+
+            IPEndPoint ie = new IPEndPoint(IPAddress.Parse(IP_Server), 31416);
+            Console.WriteLine("Starting client. Press a key to init connection");
+            Console.ReadKey();
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    s.Connect(ie);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Error connection: {0}\nError code: {1}({2})",e.Message, (SocketError)e.ErrorCode, e.ErrorCode);
+                    Console.ReadKey();
+                    return;
+                }
+                using (NetworkStream ns = new NetworkStream(s))
+                using(StreamReader sIn=new StreamReader(ns))
+                using(StreamWriter sOut=new StreamWriter(ns))
+                {
+                    msg = sIn.ReadLine();
+                    Console.WriteLine(msg);
+                    while (true)
+                    {
+                        userMsg = Console.ReadLine();
+                        sOut.WriteLine(userMsg);
+                        sOut.Flush();
+                        msg = sIn.ReadLine();
+                        Console.WriteLine(msg);
+                    }
+
+                }
+                Console.WriteLine("Ending connection");
+            }
+            
+        }
+    }
+}
---
+++ PruebasServidor/PruebasServidor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebasServidor
+{
+    class Program
+    {
+        static void ShowNetInfo(string name)
+        {
+            IPHostEntry hostInfo;
+            hostInfo = Dns.GetHostEntry(name);
+            Console.WriteLine("Name: {0}", hostInfo.HostName);
+            Console.WriteLine("IP list:");
+            foreach (IPAddress ip in hostInfo.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine("\t{0,16}", ip);
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+        static void ShowNetInfo(IPAddress ipAddress)
+        {
+            IPHostEntry hostInfo = Dns.GetHostEntry(ipAddress);
+            ShowNetInfo(hostInfo.HostName);
+        }
+        static void Main(string[] args)
+        {
+            String localHost = Dns.GetHostName();
+            Console.WriteLine("Localhost name: {0}", localHost);
+            ShowNetInfo(localHost);
+            ShowNetInfo("www.google.es");
+            ShowNetInfo(IPAddress.Parse("82.98.160.124"));
+            Console.ReadKey();
+        }
+    }
+}
